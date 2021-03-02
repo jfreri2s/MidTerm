b0625687d83d96427277c6d2f408ac72f664209f
mariobrother99 <m.hoenighausen@gmail.com>
2021-03-02 08:14:18 +0100

    Test Commit von Mario

--- uebung4/Main.java
+++ uebung4/Main.java
@@ -7,10 +7,6 @@ import org.hbrs.se.ws20.uebung4.View.Eingabe;
 
 public class Main {
 
-    public Main(){
-
-    }
-
     public static void main(String[] args) {
         Container c = Container.getInstance();
         Eingabe eingabe = new Eingabe();
---
+++ uebung4/MariosGitTest.java
@@ -0,0 +1,7 @@
+package uebung4;
+
+public class MariosGitTest {
+    public MariosGitTest() {
+        System.out.println("Was für miese undurchsichtige Nachrichten man doch so von der präferierten IDE erhält");
+    }
+}
