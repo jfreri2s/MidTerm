182fa78b68eb0b025c0af777fcac76a307efdc6c
mariobrother99 <m.hoenighausen@gmail.com>
2021-03-04 16:42:17 +0100

    Missed StoreCommand was added.

---
+++ src/Controller/StoreCommand.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import Model.Container;
+
+public class StoreCommand implements Command{
+    private Container c;
+    @Override
+    public void execute(String[] args) {
+
+    }
+}
