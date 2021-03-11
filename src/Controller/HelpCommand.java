2ace81f85a66badd5ed33112a3aab2b6ede91232
jfreri2s <frericks.jan@gmx.de>
2021-03-11 16:10:45 +0100

    help command

--- src/Controller/HelpCommand.java
+++ src/Controller/HelpCommand.java
@@ -8,6 +8,6 @@ public class HelpCommand implements Command{
     private Container c = Container.getInstance();
     @Override
     public void execute(List<String> args) {
-        //TODO: call of the according help method in container
+        c.help();
     }
 }
