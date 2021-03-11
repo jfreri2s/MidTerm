93a3a8a72d80259fb6ecfc9c4239be2dd079c89f
jfreri2s <frericks.jan@gmx.de>
2021-03-11 14:54:34 +0100

    Changed to Quality list

--- src/Model/AnalyzeConcrete.java
+++ src/Model/AnalyzeConcrete.java
@@ -22,7 +22,7 @@ public class AnalyzeConcrete implements AnalyzeStrategy{
         String[] uST = null;
         LinkedList<String> hs = new LinkedList<String>(); // list of hints for the user
         LinkedList<String> ds =  new LinkedList<String>();; // list of userstory details
-        int quality = 100;
+        LinkedList<Integer> qs= new LinkedList<>(); // list of quality
         Userstory cur = null;
 
         for(Userstory us : uSL){
@@ -52,7 +52,7 @@ public class AnalyzeConcrete implements AnalyzeStrategy{
         }
         */
         if(! uST[2].toLowerCase().equals("möchte")){
-            quality -=5;
+            qs.add(-5);
             hs.add("Es wurde kein");
             ds.add("");
         }
@@ -61,13 +61,13 @@ public class AnalyzeConcrete implements AnalyzeStrategy{
             if(uST[idx].equals("um") ){
                 if(idx < 6){
                     // too short or not sufficient information supplied
-                    quality -=5;
+                    qs.add(-5);
                     hs.add("");
                     ds.add("");
                 }
                 if(idx > 10){
                     // too long
-                    quality -=5;
+                    qs.add(-5);
                     hs.add("");
                     ds.add("");
                 }
@@ -76,27 +76,27 @@ public class AnalyzeConcrete implements AnalyzeStrategy{
             }
             else{
                 //hint - kein Mehrwert vorhanden
-                quality -=0;
+                qs.add(-5);
                 hs.add("");
                 ds.add("");
             }
             if(uST[idx].endsWith(".")){
                 if(idx < 13){
                     // hint - zu geringer Mehrwert
-                    quality -=0;
+                    qs.add(-5);
                     hs.add("");
                     ds.add("");
                 }
                 if(idx > 23){
                     // hint zu großer Mehwert
-                    quality -=0;
+                    qs.add(-5);
                     hs.add("");
                     ds.add("");
                 }
             }
 
         }
-        System.out.println("Die User Story mit der ID "+ cur.getId()+" hat folgende Qualität: " + quality);
+        System.out.println("Die User Story mit der ID "+ cur.getId()+" hat folgende Qualität: " + qs.get(0));
         //TODO what to do about the persistent storage of actors?
 
     }
