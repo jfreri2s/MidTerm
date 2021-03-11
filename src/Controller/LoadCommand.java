bd93b809de017d16a3e5466a271c1a1e22c1fd34
jfreri2s <frericks.jan@gmx.de>
2021-03-11 16:32:58 +0100

    implementation of load command

--- src/Controller/LoadCommand.java
+++ src/Controller/LoadCommand.java
@@ -1,17 +1,30 @@
 package Controller;
 
 import Model.Container;
+import Model.ContainerException;
+import Model.PersistenceException;
 
 import java.util.List;
 
-public class LoadCommand implements Command{
+public class LoadCommand implements Command {
     private Container c = Container.getInstance();
+
     @Override
     public void execute(List<String> args) {
         //TODO: call of the according load method in container, depending on the parameters
         //not sure whether there should be this dissection of this command?
-        if(args.get(0).equals("-") && args.get(0).equals("force")){
-
+        try {
+            if (args.get(0).equals("-") && args.get(0).equals("force")) {
+                c.loadForce();
+            }
+            if (args.get(0).equals("-") && args.get(0).equals("merge")) {
+                c.loadMerge();
+            }
+        } catch (ContainerException e) {
+            e.printStackTrace();
+        } catch (PersistenceException e) {
+            e.printStackTrace();
         }
     }
 }
+
